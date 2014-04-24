eab0a7f3444c0ca3190f79b514c015d780296506
Anton Krasov <jdwtwann.work@gmail.com>
2014-04-24 12:50:15 +0300

    SocialPerson class added

---
+++ lib/src/main/java/com/androidsocialnetworks/lib/SocialPerson.java
@@ -0,0 +1,12 @@
+package com.androidsocialnetworks.lib;
+
+public class SocialPerson {
+
+    public String id;
+    public String firstName;
+    public String lastName;
+    public String company;
+    public String position;
+    public String avatarURL;
+
+}
