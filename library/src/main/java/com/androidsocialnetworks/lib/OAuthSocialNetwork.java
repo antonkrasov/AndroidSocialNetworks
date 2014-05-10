6071b0350242162447606927e9a80819231e90a7
Anton Krasov <anton.krasov@gmail.com>
2014-05-10 23:13:30 +0300

    add OAuthSocialNetwork

---
+++ library/src/main/java/com/androidsocialnetworks/lib/OAuthSocialNetwork.java
@@ -0,0 +1,12 @@
+package com.androidsocialnetworks.lib;
+
+import android.support.v4.app.Fragment;
+
+public abstract class OAuthSocialNetwork extends SocialNetwork {
+
+    protected OAuthSocialNetwork(Fragment fragment) {
+        super(fragment);
+    }
+
+
+}
